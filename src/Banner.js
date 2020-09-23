cb960f2698e845545efb5f0fa4a828e791ddffd3
syukrontanzilah <syukrontanzilah@gmail.com>
2020-09-23 15:42:55 +0700

    date picker custom

--- src/Banner.js
+++ src/Banner.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Banner.css'
 import {Button} from '@material-ui/core'
+import Search from './Search'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+
 
 function Banner() {
+    const [showSearch, setShowSearch] = useState(false)
+
+
+
     return (
         <div className="banner">
+            <div className="banner__search">
+                {showSearch && <Search/>}
+            <Button 
+            onClick={()=> setShowSearch(!showSearch)}
+            className="banner__searchButton" variant="outlined">
+               {showSearch? "Hide": "Search Dates"}
+               {showSearch? <ExpandLessIcon/> :  <ExpandMoreIcon /> }
+              
+            </Button>
+         
+            </div>
             <div className="banner__info">
             <h2>Holiday in random destinations</h2>
             <h5>Plan a holiday with us and see the differences </h5>
             <Button variant="outlined">Explore</Button>
             </div>
+
+       
         </div>
     )
 }
